99a84ffcdaa9c90d2b256c517a55f5c3e7840e60
prem <prem@prems-MacBook-Pro.local>
2021-12-01 07:56:57 +0530

    first commit

---
+++ src/Homepage.component.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import './Homepage.style.scss';
+
+const Homepage = () => (
+    <div className = 'homepage'>
+        <div className ='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>one</h1>
+                    <span className='subtitle'>two</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>one</h1>
+                    <span className='subtitle'>two</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>one</h1>
+                    <span className='subtitle'>two</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>one</h1>
+                    <span className='subtitle'>two</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>one</h1>
+                    <span className='subtitle'>two</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default Homepage;
