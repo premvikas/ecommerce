16e4767225cbfe7846b80f99c221490f12785521
premvikas <premvikas.v@gmail.com>
2021-12-03 07:59:05 +0530

    Broken components and added styles

---
+++ src/pages/homepage/Homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Directory from '../../components/directory/directory.component';
+
+import './Homepage.style.scss';
+
+const Homepage = () => (
+    <div className = 'homepage'>
+       <Directory />
+    </div>
+)
+
+export default Homepage;
